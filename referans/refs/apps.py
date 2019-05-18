25d28d27891eafe50337091d5cf685c4db2cdaf4
kenanbolat <kenan23@gmail.com>
2019-05-18 16:01:00 +0300

    delete django

--- referans/refs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RefsConfig(AppConfig):
-    name = 'refs'
--- referans/refs/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class District(models.Model):
-    adi = models.CharField(max_length=255)
-    status = models.BooleanField(default=True)
