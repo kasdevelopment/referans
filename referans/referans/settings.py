51febbc05be9dc2397c36731b6b5bac3dd5316d4
kenanbolat <kenan23@gmail.com>
2019-05-18 15:56:15 +0300

    first model

--- referans/referans/settings.py
+++ referans/referans/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'refs.apps.RefsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- referans/refs/views.py
+++ referans/refs/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("<h3> Hello, world. </h3> "
+                        "<br>  You're at the polls index."
+                        "<hr> " )
 
