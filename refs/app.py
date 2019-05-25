286aa4862a3a61b3ef53a6b82e62f679d3e5d6dd
Arda Çetinkaya <ctnkyrd@gmail.com>
2019-05-25 21:04:31 +0300

    try auth

--- refs/app.py
+++ refs/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, render_template
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route("/")
 def hello():
