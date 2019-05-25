3f2cf4bb666f71a36a019619d8e9e3d4606c4b20
Arda <ctnkyrd@gmail.com>
2019-05-25 19:47:58 +0300

    add select for district-city-terrain

--- refs/static/js/app.js
+++ refs/static/js/app.js
@@ -12,6 +12,8 @@ $(document).ready(function(){
         } 
     });
 
+    fillCity($('#districts').val());
+
     demoData.forEach(function(data){
         // let index = dataObj.findIndex(obj => obj.district_name === data.district_name);
         dataObj.push(
@@ -22,9 +24,42 @@ $(document).ready(function(){
                 terrain: data[3]
             }
             );
-        
-        
-
     });
 
 });
+
+
+$( "#districts" ).change(function() {
+    let district = $(this).val();
+    fillCity(district);   
+  });
+
+  $( "#city" ).change(function() {
+    let district = $("#districts").val();
+    let city = $(this).val();
+    fillTerrain(district, city);
+  });
+
+function fillTerrain(district, city){
+    let setTerrain = [];
+    $('#terrain').empty();
+    demoData.forEach(function(data){
+        if(setTerrain.indexOf(data[3]) === -1 && district == data[1] && city === data[2]){
+            debugger;
+            setTerrain.push(data[3]);
+            $('#terrain').append(`<option>${data[3]}</option>`);
+        }
+    });
+}
+
+function fillCity(district){
+    let setCity = [];
+    $('#city').empty();
+    demoData.forEach(function(data){
+        if(setCity.indexOf(data[2]) === -1 && district === data[1]){
+            setCity.push(data[2]);
+            $('#city').append(`<option>${data[2]}</option>`);
+        }
+    });
+    fillTerrain(district, $('#city').val());
+}
