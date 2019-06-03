74e67b75418ca358e527162a0ce860cffeb2b792
kenanbolat <kenan23@gmail.com>
2019-06-03 11:29:14 +0300

    Add Under Construction Statements

--- refs/app.py
+++ refs/app.py
@@ -3,16 +3,11 @@ from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 
 
-# @app.route("/")
-# def hello():
-#     return "<h3> Hello World! </h3>"
-
-
 @app.route('/')
-def getDistricts():
+def get_districts():
     district = {"adi": "ankara"}
-    #return jsonify({"adi": "ankara"})
-    return  render_template('index.html', data=district)
+    # return jsonify({"adi": "ankara"})
+    return render_template('index.html', data=district)
 
 
 if __name__ == "__main__":
